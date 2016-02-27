7eec7d7d3215f6a617899d9970e704a4dcbf3a61
Michael Nickey <mnickey@gmail.com>
2016-02-26 22:05:12 -0800

    added error handling endpoint and multiple TODOs

--- stamps_api.py
+++ stamps_api.py
@@ -7,8 +7,12 @@ The API is to allow for GET requests that will fetch populated data and save tha
 POST, UPDATE & DELETE requests will come as the data categories are flushed out.
 authored by Michael Nickey on February 26th 2016
 """
+# Todo(mnickey) : create database to hold tasks -- top priority
+# Todo(mnickey) : create POST request endpoint
+# Todo(mnickey) : create UPDATE endpoint
+# Todo(mnickey) : create DELETE endpoint
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 import logging
 
 app = Flask(__name__)
@@ -46,6 +50,12 @@ def get_stamps_by_id(stamp_id):
     if len(stamp) == 0:
         abort(404)
     return jsonify({'stamps': stamp})
+
+
+# Error handling for stamps not found by API request
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
