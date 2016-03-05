3ef77bdfc519710078f616643af8780f445f1640
Michael Nickey <mnickey@gmail.com>
2016-03-04 21:54:57 -0800

    Added all states to database, confimred that all API endpoints are working properly.

--- database.py
+++ database.py
@@ -2,7 +2,6 @@
 
 # Database Configuration
 import os
-from migrate.versioning import api
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,17 +9,10 @@ from sqlalchemy import Column, String, Integer, Float
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'stamps.db')
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=False)
 Base = declarative_base()
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-# if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-#     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
-#     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
-# else:
-#     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
-#                         api.version(SQLALCHEMY_MIGRATE_REPO))
 
 class Stamp(Base):
     __tablename__ = 'stamp'
@@ -35,7 +27,7 @@ class Stamp(Base):
     value = Column(Float(10))
 
     def to_dict(self):
-        return {"id": self.id, "state": self.state,
+        return {"id": self.id, "state": self.state, "stamp_url": self.stamp_url,
                 "state_abbr": self.state_abbr, "stamp_name": self.stamp_name,
                 "blurb": self.blurb, "date_issued": self.date_issued,
                 "country": self.country, "value": self.value}
